ffeffd96ed36600d1183a9e51be8428754b095c8
Johyan <johyan_carranza@outlook.com>
2024-05-01 17:47:59 -0600

    Adds Firebase tools

--- shoppy.bz/src/App.js
+++ shoppy.bz/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import react from "react";
 import './App.css';
 
 function App() {
   return (
+    // BEM
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, this is the initial build 😊😊😊</h1>
+      /* Header */
+      /* Home */
     </div>
   );
 }
