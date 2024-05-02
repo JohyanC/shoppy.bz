50ac708c3c0a076cd1018403e69da529c63a9310
Johyan <johyan_carranza@outlook.com>
2024-05-01 19:13:48 -0600

    Updates Material Icons & disables search icon

--- shoppy.bz/src/Header.js
+++ shoppy.bz/src/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Header.css'
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Header() {
   return (
@@ -13,23 +13,23 @@ function Header() {
       <div className='header_search'>
         <input className='header_searchInput'
         type='text' />
-        <SearchIcon className='header_searchIcon'/>
+        {/*<SearchIcon className='header_searchIcon'/>*/}
       </div>
 
       <div className='header_nav'>
         <div className='header_option'>
             <span className='header_optionLineOne'>Hello Guest</span>
-            <span className='header_optionLineOne'>Sign In</span>
+            <span className='header_optionLineTwo'>Sign In</span>
         </div>
         
         <div className='header_option'>
             <span className='header_optionLineOne'>Returns</span>
-            <span className='header_optionLineOne'>& Orders</span>
+            <span className='header_optionLineTwo'>& Orders</span>
         </div>
         
         <div className='header_option'>
             <span className='header_optionLineOne'>Your</span>
-            <span className='header_optionLineOne'>Cart</span>
+            <span className='header_optionLineTwo'>Cart</span>
         </div>
 
       </div>
