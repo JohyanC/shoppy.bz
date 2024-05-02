a7e394f53d37b94eedd5b71c4211c02f14d1382c
Johyan <johyan_carranza@outlook.com>
2024-05-01 18:58:38 -0600

    Updates nav bar

--- shoppy.bz/src/App.js
+++ shoppy.bz/src/App.js
@@ -5,7 +5,7 @@ import Header from './Header';
 function App() {
   return (
     // BEM
-    <div className="App">
+    <div className="app">
       <Header />
       {/* Home */}
     </div>
--- shoppy.bz/src/Header.js
+++ shoppy.bz/src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
   return (
@@ -12,7 +13,7 @@ function Header() {
       <div className='header_search'>
         <input className='header_searchInput'
         type='text' />
-        {/* Logo */}
+        <SearchIcon className='header_searchIcon'/>
       </div>
 
       <div className='header_nav'>
