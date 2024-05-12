ae56508c76e4e48a0e93a3a4a6786e5d7763dffc
Johyan Carranza <johyan_carranza@outlook.com>
2024-05-12 17:07:53 -0600

    Updates header with new logo

--- shoppy.bz/src/Header.js
+++ shoppy.bz/src/Header.js
@@ -2,9 +2,10 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import logo from './logo.png';
+import logo from './logoSmall.jpeg';
 import { Link } from 'react-router-dom';
 import { useStateValue } from './StateProvider';
+import { Outlet } from 'react-router-dom';
 
 function Header() {
     const [{basket}, dispatch] = useStateValue();
@@ -14,48 +15,53 @@ function Header() {
       <Link to='/'>
         <img classname='header_logo'
           src={logo}
-          alt='shoppy logo'
-        />
+          alt='shoppy logo' />
       </Link>
-        
+
 
       <div className='header_search'>
         <input className='header_searchInput'
-        type='text' />
-        <SearchIcon className='header_searchIcon'/>
+          type='text' />
+        <SearchIcon className='header_searchIcon' />
       </div>
 
       <div className='header_nav'>
-        <Link to='/Login' className='text-link'>
+        <Link to='login' className='text-link'>
           <div className='header_option'>
-              <span className='header_optionLineOne'>Hello Guest</span>
-              <span className='header_optionLineTwo'>Sign In</span>
+            <span className='header_optionLineOne'>Hello Guest</span>
+            <span className='header_optionLineTwo'>Sign In</span>
           </div>
         </Link>
-      <Link className='text-link'>
-         <div className='header_option'>
+        <Link className='text-link'>
+          <div className='header_option'>
             <span className='header_optionLineOne'>Returns</span>
             <span className='header_optionLineTwo'>& Orders</span>
-        </div>
-      </Link>
-        
-        
+          </div>
+        </Link>
 
-        <Link to='/checkout' className='text-link'>
-          <div className='header_optionBasket'>            
+
+
+        <Link to='checkout' className='text-link'>
+          <div className='header_optionBasket'>
             <div className='header_option'>
-                <span className='header_optionLineOne'>Your</span>
-                <span className='header_optionLineTwo'>Cart</span>
+              <span className='header_optionLineOne'>Your</span>
+              <span className='header_optionLineTwo'>Cart</span>
             </div>
-            
-            <ShoppingCartIcon/>
+
+            <ShoppingCartIcon />
             <span className='header_optionLineTwo header_basketCount'>
               {basket?.length}</span>
           </div>
         </Link>
 
       </div>
-    </div>
+      <Outlet/>
+      {/*</div><div className='footer'>
+        This is the footer
+  </div>*/}
+  </div>
+
+
   )
 }
 
